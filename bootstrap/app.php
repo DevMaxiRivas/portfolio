8501d8a8a1b3fc59000d3bfc7899c5944ae12ecb
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-01-20 11:42:37 -0300

    Add Trust Proxy Settings

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(
             SetLocaleMiddleware::class,
         );
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
